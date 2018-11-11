8ffd85b19ba4ca011a4edc501a7dc8c861ecac02
Steven Tran <stran050@ucr.edu>
2018-11-11 14:10:24 -0800

    created button base

--- popup.js
+++ popup.js
@@ -4,14 +4,14 @@ let changeColor = document.getElementById('changeColor');
     changeColor.style.backgroundColor = data.color;
     changeColor.setAttribute('value', data.color);
   });
-  changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
-  };
+  // changeColor.onclick = function(element) {
+  //   let color = element.target.value;
+  //   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  //     chrome.tabs.executeScript(
+  //         tabs[0].id,
+  //         {code: 'document.body.style.backgroundColor = "' + color + '";'});
+  //   });
+  // };
   chrome.tabs.getSelected(null,function(tab) { // null defaults to current window
     var title = tab.title;
     document.getElementById("test").value = title;
